a20e5443f39bb394e6905eb2268c76ef51cf6f67
Piotr Fusik <fox@scene.pl>
2013-10-10 23:24:39 +0200

    [c64] Add basic C64 formats.

--- win32/thumbrecoil/thumbrecoil.cpp
+++ win32/thumbrecoil/thumbrecoil.cpp
@@ -91,19 +91,22 @@ DECLARE_INTERFACE_(IThumbnailProvider, IUnknown)
 static const char extensions[][6] =
 	{ ".256", ".4mi", ".4pl", ".4pm", ".a4r", ".acbm", ".acs", ".agp", ".all", ".ap2",
 	  ".ap3", ".apc", ".apl", ".app", ".apv", ".art", ".atr", ".bg9", ".bkg", ".bl1",
-	  ".bl2", ".bl3", ".bru", ".ca1", ".ca2", ".ca3", ".cci", ".ch4", ".ch6", ".ch8",
-	  ".chr", ".cin", ".cpr", ".cpt", ".dc1", ".del", ".dg1", ".dgc", ".dgp", ".dgu",
-	  ".din", ".dlm", ".doo", ".dph", ".drg", ".esc", ".fnt", ".ftc", ".fwa", ".g09",
-	  ".g10", ".g11", ".gfb", ".ghg", ".god", ".gr7", ".gr8", ".gr9", ".hip", ".hlr",
-	  ".hpm", ".hr",  ".hr2", ".ice", ".icn", ".iff", ".ifl", ".ige", ".ilc", ".img",
-	  ".imn", ".ing", ".inp", ".int", ".ip2", ".ipc", ".ir2", ".irg", ".ist", ".jgp",
-	  ".lbm", ".leo", ".max", ".mbg", ".mc",  ".mch", ".mcp", ".mcs", ".mg1", ".mg2",
-	  ".mg4", ".mg8", ".mgp", ".mic", ".mis", ".mpp", ".neo", ".nlq", ".pac", ".pc1",
-	  ".pc2", ".pc3", ".pcs", ".pgc", ".pgf", ".pi1", ".pi2", ".pi3", ".pi4", ".pi9",
-	  ".pic", ".pla", ".plm", ".pmd", ".pzm", ".raw", ".rgb", ".rip", ".rm0", ".rm1",
-	  ".rm2", ".rm3", ".rm4", ".scr", ".sge", ".shc", ".shp", ".spc", ".sps", ".spu",
-	  ".sxs", ".tip", ".tn1", ".tn2", ".tn3", ".tny", ".trp", ".tru", ".vzi", ".wnd",
-	  ".ximg", ".xlp", ".zxp" };
+	  ".bl2", ".bl3", ".bru", ".ca1", ".ca2", ".ca3", ".cci", ".cdu", ".ch4", ".ch6",
+	  ".ch8", ".che", ".chr", ".cin", ".cpr", ".cpt", ".cwg", ".dc1", ".dd",  ".del",
+	  ".dg1", ".dgc", ".dgp", ".dgu", ".din", ".dlm", ".dol", ".doo", ".dph", ".drg",
+	  ".drz", ".esc", ".fgs", ".fnt", ".fpt", ".ftc", ".fwa", ".g09", ".g10", ".g11",
+	  ".gcd", ".gfb", ".ghg", ".gig", ".gih", ".god", ".gr7", ".gr8", ".gr9", ".hbm",
+	  ".hed", ".hip", ".hir", ".hlr", ".hpm", ".hr",  ".hr2", ".ice", ".icn", ".iff",
+	  ".ifl", ".ige", ".ilc", ".img", ".imn", ".ing", ".inp", ".int", ".ip2", ".ipc",
+	  ".iph", ".ipt", ".ir2", ".irg", ".ism", ".ist", ".jgp", ".koa", ".lbm", ".leo",
+	  ".max", ".mbg", ".mc",  ".mch", ".mcp", ".mcs", ".mg1", ".mg2", ".mg4", ".mg8",
+	  ".mgp", ".mic", ".mil", ".mis", ".mon", ".mpp", ".neo", ".nlq", ".ocp", ".p64",
+	  ".pac", ".pc1", ".pc2", ".pc3", ".pcs", ".pgc", ".pgf", ".pi",  ".pi1", ".pi2",
+	  ".pi3", ".pi4", ".pi9", ".pic", ".pla", ".plm", ".pmd", ".pmg", ".pzm", ".raw",
+	  ".rgb", ".rip", ".rm0", ".rm1", ".rm2", ".rm3", ".rm4", ".rp",  ".rpm", ".sar",
+	  ".scr", ".sge", ".shc", ".shp", ".spc", ".sps", ".spu", ".sxs", ".tip", ".tn1",
+	  ".tn2", ".tn3", ".tny", ".trp", ".tru", ".vid", ".vzi", ".wnd", ".ximg", ".xlp",
+	  ".zxp" };
 #define N_EXTS (int) (sizeof(extensions) / sizeof(extensions[0]))
 
 static HINSTANCE g_hDll;
