957ebc7d08a0ed859a6d08cfa8ab0359169b27f1
Piotr Fusik <fox@scene.pl>
2017-04-20 15:54:43 +0200

    [png] Process next files after an error.

--- recoil2png.c
+++ recoil2png.c
@@ -1,7 +1,7 @@
 /*
  * recoil2png.c - command-line converter of retro computer pictures to the PNG format
  *
- * Copyright (C) 2009-2015  Piotr Fusik and Adrian Matoga
+ * Copyright (C) 2009-2017  Piotr Fusik and Adrian Matoga
  *
  * This file is part of RECOIL (Retro Computer Image Library),
  * see http://recoil.sourceforge.net
@@ -22,15 +22,10 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
 
 #include "recoil-stdio.h"
 #include "pngsave.h"
-
-static RECOIL *recoil = NULL;
-static const char *output_file = NULL;
 
 static void print_help(void)
 {
@@ -44,42 +39,47 @@ static void print_help(void)
 	);
 }
 
-static void fatal_error(const char *format, ...)
-{
-	va_list args;
-	va_start(args, format);
-	fprintf(stderr, "recoil2png: ");
-	vfprintf(stderr, format, args);
-	fputc('\n', stderr);
-	va_end(args);
-	exit(1);
-}
-
 static int load_file(const char *filename, void *buffer, size_t buffer_len)
 {
 	FILE *fp = fopen(filename, "rb");
 	int len;
-	if (fp == NULL)
-		fatal_error("cannot open %s", filename);
+	if (fp == NULL) {
+		fprintf(stderr, "recoil2png: cannot open %s\n", filename);
+		return -1;
+	}
 	len = fread(buffer, 1, buffer_len, fp);
 	fclose(fp);
 	return len;
 }
 
-static void load_palette(const char *filename)
+static cibool load_palette(RECOIL *recoil, const char *filename)
 {
 	unsigned char atari8_palette[768 + 1];
-	if (load_file(filename, atari8_palette, sizeof(atari8_palette)) != 768)
-		fatal_error("%s: palette file must be 768 bytes", filename);
-	RECOIL_SetAtari8Palette(recoil, atari8_palette);
+	switch (load_file(filename, atari8_palette, sizeof(atari8_palette))) {
+	case 768:
+		RECOIL_SetAtari8Palette(recoil, atari8_palette);
+		return TRUE;
+	case -1:
+		/* error already printed */
+		return FALSE;
+	default:
+		fprintf(stderr, "recoil2png: %s: palette file must be 768 bytes long\n", filename);
+		return FALSE;
+	}
 }
 
-static void process_file(const char *input_file)
+static cibool process_file(RECOIL *recoil, const char *input_file, const char *output_file)
 {
 	static unsigned char content[RECOIL_MAX_CONTENT_LENGTH];
 	int content_len = load_file(input_file, content, sizeof(content));
-	if (!RECOIL_Decode(recoil, input_file, content, content_len))
-		fatal_error("%s: file decoding error", input_file);
+	if (content_len < 0) {
+		/* error already printed */
+		return FALSE;
+	}
+	if (!RECOIL_Decode(recoil, input_file, content, content_len)) {
+		fprintf(stderr, "recoil2png: %s: file decoding error\n", input_file);
+		return FALSE;
+	}
 
 	if (output_file == NULL) {
 		static char output_default[FILENAME_MAX];
@@ -91,32 +91,43 @@ static void process_file(const char *input_file)
 		strcpy(output_default + (dotp == 0 ? i : dotp), ".png");
 		output_file = output_default;
 	}
-	if (!RECOIL_SavePng(recoil, output_file))
-		fatal_error("cannot write %s", output_file);
-	output_file = NULL;
+	if (!RECOIL_SavePng(recoil, output_file)) {
+		fprintf(stderr, "recoil2png: cannot write %s\n", output_file);
+		return FALSE;
+	}
+	return TRUE;
 }
 
 int main(int argc, char **argv)
 {
+	RECOIL *recoil = RECOILStdio_New();
+	const char *output_file = NULL;
+	cibool ok = TRUE;
 	cibool no_input_files = TRUE;
 	int i;
-	recoil = RECOILStdio_New();
-	if (recoil == NULL)
-		fatal_error("recoil2png: out of memory");
+	if (recoil == NULL) {
+		fprintf(stderr, "recoil2png: out of memory\n");
+		return 1;
+	}
 	for (i = 1; i < argc; i++) {
 		const char *arg = argv[i];
 		if (arg[0] != '-') {
-			process_file(arg);
+			ok &= process_file(recoil, arg, output_file);
 			no_input_files = FALSE;
+			output_file = NULL;
 		}
-		else if (arg[1] == 'o' && arg[2] == '\0')
+		else if (arg[1] == 'o' && arg[2] == '\0' && i + 1 < argc)
 			output_file = argv[++i];
 		else if (strncmp(arg, "--output=", 9) == 0)
 			output_file = arg + 9;
-		else if (arg[1] == 'p' && arg[2] == '\0')
-			load_palette(argv[++i]);
-		else if (strncmp(arg, "--palette=", 10) == 0)
-			load_palette(arg + 10);
+		else if (arg[1] == 'p' && arg[2] == '\0' && i + 1 < argc) {
+			if (!load_palette(recoil, argv[++i]))
+				return 1;
+		}
+		else if (strncmp(arg, "--palette=", 10) == 0) {
+			if (!load_palette(recoil, arg + 10))
+				return 1;
+		}
 		else if ((arg[1] == 'h' && arg[2] == '\0')
 			|| strcmp(arg, "--help") == 0) {
 			print_help();
@@ -127,12 +138,14 @@ int main(int argc, char **argv)
 			printf("recoil2png " RECOIL_VERSION "\n");
 			no_input_files = FALSE;
 		}
-		else
-			fatal_error("unknown option: %s", arg);
+		else {
+			fprintf(stderr, "recoil2png: unknown option: %s\n", arg);
+			return 1;
+		}
 	}
 	if (no_input_files) {
 		print_help();
 		return 1;
 	}
-	return 0;
+	return ok ? 0 : 1;
 }
