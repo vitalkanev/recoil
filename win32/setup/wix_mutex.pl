16473f89053377a135befc527632eb054d5bd84f
Piotr Fusik <fox@scene.pl>
2018-05-05 21:36:10 +0200

    [xnviewmp] Add to Windows setup.

--- win32/setup/wix_mutex.pl
+++ win32/setup/wix_mutex.pl
@@ -1,5 +1,5 @@
 # Generate mutual exclusion conditions
-@c = qw(recoilwin.ext xnview_recoil.ext imagine.ext imagine64.ext);
+@c = qw(recoilwin.ext xnview_recoil.ext xnviewmp64.ext imagine.ext imagine64.ext);
 @a = map "\$$_=3 OR (\$$_=-1 AND ?$_=3)", @c; # action is install or (action is nothing and already installed)
 for $a (@a[0 .. $#a - 1]) {
 	@b = @a[++$i .. $#a];
