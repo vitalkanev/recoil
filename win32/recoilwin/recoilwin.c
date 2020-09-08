325b87577ddc72726ee1b8d481f6480d0dac23c1
Piotr Fusik <fox@scene.pl>
2020-09-08 14:11:12 +0200

    [recoilwin] Change icon about text.

--- win32/recoilwin/recoilwin.c
+++ win32/recoilwin/recoilwin.c
@@ -79,7 +79,7 @@ static void ShowAbout(void)
 		"Retro Computer Image Library\n"
 		"(C) " RECOIL_YEARS " Piotr Fusik and Adrian Matoga\n"
 		"Formats research, testing by Mariusz Rozwadowski\n"
-		"RECOIL icon (C) 2014 Pawel Szewczyk\n\n"
+		"Icon by Pawel Szewczyk\n\n"
 		RECOIL_COPYRIGHT,
 		APP_TITLE " " RECOIL_VERSION,
 		MB_OK | MB_USERICON,
