248c45806290a05ed0a210ce9ea98d0c57e3e65d
Piotr Fusik <fox@scene.pl>
2021-01-01 14:41:12 +0100

    [doc] Bump the copyright year.

--- uwp/AssemblyInfo.cs
+++ uwp/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RECOIL")]
 [assembly: AssemblyCompany("Piotr Fusik")]
 [assembly: AssemblyProduct("RECOIL")]
-[assembly: AssemblyCopyright("Copyright © 2016-2020")]
+[assembly: AssemblyCopyright("Copyright © 2016-2021")]
 [assembly: AssemblyVersion(Recoil.RECOIL.Version + ".0")]
 [assembly: AssemblyFileVersion(Recoil.RECOIL.Version + ".0")]
 [assembly: ComVisible(false)]
